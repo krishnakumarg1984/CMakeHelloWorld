e714fa1a6cbd976d8d97e30291d94375fb834adf
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-01-19 19:40:22 +0000

    following hackmd notes, minor change to hello.cpp for recompilation

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   std::cout << "Hello, World!" << std::endl;
+  std::cout << "Change made by Krishna\n";
   return 0;
 }
