7ee14d7050c12dce9ad1b7eebe890b66ecd0eabe
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-01-19 19:52:38 +0000

    renames hi.cpp to nice.cpp and changes CMakeLists.txt accordingly; will automatically trigger a cmake cycle as needed

--- hi.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main(int argc, char **argv)
-{
-  std::cout << "Hi, World!" << std::endl;
-  std::cout << "Change made by Krishna\n";
-  return 0;
-}
---
+++ nice.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+  std::cout << "Nice to meet you\n";
+  return 0;
+}
